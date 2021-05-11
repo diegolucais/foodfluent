9edd2b69b4a835348a203e999b6da1bf2f2c812b
Diego Lucas <diegolucas@outlook.ie>
2021-05-11 21:11:22 +0100

    Added About page + general improvements

--- javascript/random-user-api.js
+++ javascript/random-user-api.js
@@ -1,36 +1,35 @@
 function randomUsers() {
 
-    // Get API
+    // Get API with the number of results to return
     fetch('https://randomuser.me/api/?results=5')
         .then(res => res.json())
         .then(data => {
 
-            let author = data.results;
+            let people = data.results;
 
             // Get data value
-            let output = '<h1 class="welcome-users"><center>Welcome to FoodFluent</center></h1>';
+            let output = '<h1 class="welcome-users">Welcome to FoodFluent</h1>';
 
             // Get data loop through
-            author.forEach(function (lists) {
+            people.forEach(function (lists) {
                 output += `
-                <div class="container">
-                    <div class="card mt-4 bg-light">
+                    <div class="card mt-3 bg-dark">
                         <ul class="list-group">
-                            <li class="list-group-item"><h4><strong>Name:</strong> ${lists.name.first} ${lists.name.last}</h4></li>
+                            <li class="list-group-item"><h5><strong>Name:</strong> ${lists.name.first} ${lists.name.last}</h5></li>
                             <li class="list-group-item"><img src="${lists.picture.large}"></li>
                             <li class="list-group-item"><strong>Phone Number:</strong> ${lists.cell}</li>
                             <li class="list-group-item"><strong>Email:</strong> ${lists.email}</li>
                             <li class="list-group-item"><strong>Address:</strong> ${lists.location.city}, ${lists.location.country} | <strong>Post Code:</strong> ${lists.location.postcode}</li>
                         </ul>
                     </div>
-                </div> `;
+                `;
             });
 
             // Show all data received within the HTML element
-            document.getElementById('output').innerHTML = output;
+            document.getElementById("output").innerHTML = output;
 
         });
 };
 
-// Users will appear randomly on page load
+// Users will appear randomly every time the page is loaded
 window.onload = randomUsers;
