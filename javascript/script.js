9b0f6ae49e8ddab29457f1e600f4bbd84c88c29a
Diego Lucas <diegolucas@outlook.ie>
2021-05-12 20:59:58 +0100

    General fixes and improvements

--- javascript/script.js
+++ javascript/script.js
@@ -1,72 +1,120 @@
-/* Password Validator */
+/* ***********************************************
+Password Validator
+*********************************************** */
+
 $(document).ready(function() {
-     $('input[type=password]').keyup(function() {
-          var pswd = $(this).val();
-          
-          // Lower case letters validation
-          if (pswd.match(/[a-z]/)) {
-               $('#letter').removeClass('invalid').addClass('valid');
-          }
-          else {
-               $('#letter').removeClass('valid').addClass('invalid');
-          }
+  $('input[type=password]').keyup(function() {
+       var pswd = $(this).val();
+       
+       // Lower case letters validation
+       if (pswd.match(/[a-z]/)) {
+            $('#letter').removeClass('invalid').addClass('valid');
+       }
+       else {
+            $('#letter').removeClass('valid').addClass('invalid');
+       }
 
-          // Upper case letters validation
-          if (pswd.match(/[A-Z]/)) {
-               $('#capital').removeClass('invalid').addClass('valid');
-          }
-          else {
-               $('#capital').removeClass('valid').addClass('invalid');
-          }
+       // Upper case letters validation
+       if (pswd.match(/[A-Z]/)) {
+            $('#capital').removeClass('invalid').addClass('valid');
+       }
+       else {
+            $('#capital').removeClass('valid').addClass('invalid');
+       }
 
-          // Numbers validation
-          if (pswd.match(/\d/)) {
-               $('#number').removeClass('invalid').addClass('valid');
-          }
-          else {
-               $('#number').removeClass('valid').addClass('invalid');
-          }
+       // Numbers validation
+       if (pswd.match(/\d/)) {
+            $('#number').removeClass('invalid').addClass('valid');
+       }
+       else {
+            $('#number').removeClass('valid').addClass('invalid');
+       }
 
-          // Special characters validation
-          if (pswd.match(/[^A-Za-z0-9]/)) {
-               $('#symbol').removeClass('invalid').addClass('valid');
-          }
-          else {
-               $('#symbol').removeClass('valid').addClass('invalid');
-          }
+       // Special characters validation
+       if (pswd.match(/[^A-Za-z0-9]/)) {
+            $('#symbol').removeClass('invalid').addClass('valid');
+       }
+       else {
+            $('#symbol').removeClass('valid').addClass('invalid');
+       }
 
-          // Length validation
-          if (pswd.length < 8) {
-               $('#length').removeClass('valid').addClass('invalid');
-          }
-          else {
-               $('#length').removeClass('invalid').addClass('valid');
-          }
-          
-     // Show password requirements whenever user clicks in the password field
-     }).focus(function() {
-          $('#password-info').show();
-     }).blur(function() {
-          $('#password-info').hide();
-     });
+       // Length validation
+       if (pswd.length < 8) {
+            $('#length').removeClass('valid').addClass('invalid');
+       }
+       else {
+            $('#length').removeClass('invalid').addClass('valid');
+       }
+       
+  // Show password requirements whenever user clicks in the password field
+  }).focus(function() {
+       $('#password-info').show();
+  }).blur(function() {
+       $('#password-info').hide();
+  });
 });
 
 /* Login button action */
 function welcome() {
-     //Variables to save username and password
-     var username = document.getElementById("username").value;
-     var password = document.getElementById("password").value;
-     
-     // Show welcome message if user entered username, and password meets the requirements
-     if (username != 0 && password.match(/[a-z]/) && password.match(/[A-Z]/) && password.match(/\d/) && password.match(/[^A-Za-z0-9]/) && password.length >= 8) {
-          window.alert("Welcome " + username + "!");
-     }
-     // Show message if user doesn't type anything on both username and password
-     else if (username == 0 || password == 0) {
-          window.alert("Enter a username and a password, please.");
-     }
-     // Show message if password doesn't meet the requirements
-     else {
-          window.alert("The password you entered is either invalid or does not meet the requirements! Please, try again.")
-     }
+  //Variables to save username and password
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  // Show welcome message if user entered username, and password meets the requirements
+  if (username != 0 && password.match(/[a-z]/) && password.match(/[A-Z]/) && password.match(/\d/) && password.match(/[^A-Za-z0-9]/) && password.length >= 8) {
+       window.alert("Welcome " + username + "!");
+  }
+  // Show message if user doesn't type anything on both username and password
+  else if (username == 0 || password == 0) {
+       window.alert("Enter a username and a password, please.");
+  }
+  // Show message if password doesn't meet the requirements
+  else {
+       window.alert("The password you entered is either invalid or does not meet the requirements! Please, try again.")
+  }
 }
+
+/* ***********************************************
+Random User API
+*********************************************** */
+
+function randomUsers() {
+
+    // Get API with the number of results to return
+    fetch('https://randomuser.me/api/?results=5')
+        .then(res => res.json())
+        .then(data => {
+
+            let people = data.results;
+
+            // Get data value
+            let output = '<h1 class="welcome-users">Welcome to FoodFluent</h1>';
+
+            // Get data loop through
+            people.forEach(function (lists) {
+                output += `
+                    <div class="card mt-3 bg-dark">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h5><strong>Name:</strong> ${lists.name.first} ${lists.name.last}</h5></li>
+                            <li class="list-group-item"><img src="${lists.picture.large}"></li>
+                            <li class="list-group-item"><strong>Phone Number:</strong> ${lists.cell}</li>
+                            <li class="list-group-item"><strong>Email:</strong> ${lists.email}</li>
+                            <li class="list-group-item"><strong>Address:</strong> ${lists.location.city}, ${lists.location.country} | <strong>Post Code:</strong> ${lists.location.postcode}</li>
+                        </ul>
+                    </div>
+                `;
+            });
+
+            // Show all data received within the HTML element
+            document.getElementById("output").innerHTML = output;
+
+        });
+};
+
+// Users will appear randomly every time the page is loaded
+window.onload = randomUsers;
+
+/* ***********************************************
+Shopping Cart
+*********************************************** */
+
